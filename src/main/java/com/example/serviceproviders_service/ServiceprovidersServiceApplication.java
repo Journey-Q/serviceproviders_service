0e106a80aa9bdce2d3cf37ce785b6615877fb52e
Sangeerth2001 <ysangeerththanasarma@gmail.com>
2025-06-27 18:00:51 +0530

    first commit

---
+++ src/main/java/com/example/serviceproviders_service/ServiceprovidersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceproviders_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceprovidersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceprovidersServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serviceproviders_service/ServiceprovidersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceproviders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceprovidersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
